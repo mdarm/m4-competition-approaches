fbffdd53cd2b90f20004926cfde226f9e6231409
Nektarios Christou <129678580+nekxt@users.noreply.github.com>
2023-09-12 18:46:04 +0300

    Update main.py

--- MLP/main.py
+++ MLP/main.py
@@ -2,70 +2,126 @@ from baseline import Baseline
 from window_generator import WindowGenerator
 from data_reader import get_data
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from matplotlib import pyplot as plt
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import warnings
+from itertools import cycle
+
 
 # Suppress the specific TensorFlow warning
 warnings.filterwarnings("ignore", message=".*AVX_VNNI FMA.*")
 
+
 def main():
 
-    # fetch datasets
-    train_df, val_df, test_df = get_data()
+    """
+    print(tf.config.list_physical_devices('GPU'))
+    gpu_device = tf.config.list_physical_devices('GPU')[0]
+    tf.config.experimental.set_memory_growth(gpu_device, True)
+
+    with tf.device('/GPU:0'):
+        # Create your TensorFlow model here
+        model = tf.keras.Sequential([...])
+    """
+
+    # fetch datasets (trend and seasonality are filtered)
+    dataset = 'Daily'
+    train_data, test_data = get_data(dataset)
+
+    # Normalize data
+    scaler_tr = MinMaxScaler()
+    scaler_ts = MinMaxScaler()
+    X_train = scaler_tr.fit_transform(train_data)
+    X_test = scaler_ts.fit_transform(test_data)
+    # Convert the NumPy arrays back to Pandas DataFrames
+    X_train = pd.DataFrame(X_train, columns=train_data.columns)
+    X_test = pd.DataFrame(X_test, columns=test_data.columns)
+
+    # Add a column with one-hot encodings of days,
+    # assuming that the first record starts from Monday.
+    encodings = [
+        [1, 0, 0, 0, 0, 0 ,0], # monday
+        [0, 1, 0, 0, 0, 0, 0], # tuesday
+        [0, 0, 1 ,0, 0, 0, 0], # wednesday
+        [0, 0 ,0, 1 ,0 ,0, 0], # thursday
+        [0, 0, 0, 0 ,1, 0, 0], # friday
+        [0, 0, 0, 0, 0, 1, 0], # saturday
+        [0, 0 ,0, 0, 0, 0, 1]  # sunday
+    ]
+    encodings_cycle = cycle(encodings)
+    new_columns = ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU']
+    # training set:
+    # add new columns
+    for column_name in reversed(new_columns):
+        X_train.insert(0, column_name, 0)
+    # replace the new columns values with the encodings
+    for index, row in X_train.iterrows():
+        encoding = next(encodings_cycle)
+        X_train.loc[index, X_train.columns[:7]] = encoding
+    # test set:
+    # add new columns
+    encodings_cycle = cycle(encodings)
+    for column_name in reversed(new_columns):
+        X_test.insert(0, column_name, 0)
+    # replace the new columns values with the encodings
+    for index, row in X_test.iterrows():
+        encoding = next(encodings_cycle)
+        X_test.loc[index, X_test.columns[:7]] = encoding
+
+    # Split the train_data into training and validation sets
+    train_ratio = 0.8
+    validation_ratio = 0.2
+    X_train, X_val = train_test_split(X_train,
+                                      test_size=validation_ratio,
+                                      random_state=42,
+                                      shuffle=False)
+
+    # Convert the NumPy arrays back to Pandas DataFrames
+    train_df = pd.DataFrame(X_train, columns=X_train.columns)
+    val_df = pd.DataFrame(X_val, columns=X_train.columns)
+    test_df = pd.DataFrame(X_test, columns=X_test.columns)
 
     # etc.
-    target_label = 'V49'
+    MAX_EPOCHS = 20
+    target_labels = ['V450']
     val_performance = {}
     performance = {}
 
-    # Single-step window
-    print("Single-step model")
-    single_step_window = WindowGenerator(
-        input_width=1, label_width=1, shift=1,
-        train_df=train_df, val_df=val_df, test_df=test_df,
-        label_columns=[target_label])
-    print(single_step_window)
-    # Multi-layer model
-    MAX_EPOCHS = 20
-    dense = tf.keras.Sequential([
-        tf.keras.layers.Dense(units=64, activation='relu'),
-        tf.keras.layers.Dense(units=64, activation='relu'),
-        tf.keras.layers.Dense(units=1)
-    ])
-    history = compile_and_fit(dense, single_step_window, MAX_EPOCHS)
-    val_performance['Dense'] = dense.evaluate(single_step_window.val)
-    single_step_window.plot(dense)
-
-    print("\n--------------------\n")
-
     # Multi-step window
     print("Multi-step model")
-    CONV_WIDTH = 3
+    CONV_WIDTH = 30
     conv_window = WindowGenerator(
-        input_width=CONV_WIDTH, label_width=1, shift=1,
+        input_width=CONV_WIDTH,
+        label_width=1,
+        shift=1,
         train_df=train_df, val_df=val_df, test_df=test_df,
-        label_columns=['V49'])
-    print(conv_window)
+        label_columns=target_labels)
     # Multi-layer model
+    #with tf.device('/GPU:0'):
     multi_step_dense = tf.keras.Sequential([
-        # Shape: (time, features) => (time*features)
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(units=32, activation='relu'),
-        tf.keras.layers.Dense(units=32, activation='relu'),
+        tf.keras.layers.Dense(units=128, activation='relu'),
+        tf.keras.layers.Dense(units=128, activation='relu'),
         tf.keras.layers.Dense(units=1),
-        # Add back the time dimension.
-        # Shape: (outputs) => (1, outputs)
         tf.keras.layers.Reshape([1, -1]),
     ])
-    print('Input shape:', conv_window.example[0].shape)
-    print('Output shape:', multi_step_dense(conv_window.example[0]).shape)
+    for example_inputs, example_labels in conv_window.train.take(1):
+        print(f'Inputs shape (batch, time, features): {example_inputs.shape}')
+        print(f'Labels shape (batch, time, features): {example_labels.shape}')
     history = compile_and_fit(multi_step_dense, conv_window, MAX_EPOCHS)
     val_performance['Multi step dense'] = multi_step_dense.evaluate(conv_window.val)
-    conv_window.plot(multi_step_dense)
+    conv_window.plot(model=multi_step_dense, plot_col=target_labels[0])
+
+    """
+    The main down-side of this approach is that the resulting model can only be executed
+    on input windows of exactly this shape.
+    """
 
 
 def compile_and_fit(model, window, max_epochs, patience=2, ):
